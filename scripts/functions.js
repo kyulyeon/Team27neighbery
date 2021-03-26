9bc33291b42d8f6fe67794964e159b49fcb77ecd
Maggie Dou <maggie.d0228@hotmail.com>
2021-03-26 09:45:38 -0700

    added css styling to reviews

--- scripts/functions.js
+++ scripts/functions.js
@@ -48,22 +48,30 @@ function reviews() {
 }
 // reviews();
 
-function reviewsQuery() {
+function showReviews() {
     db.collection("reviews")
     .orderBy("date")
     .get()
     .then(function (snap) {
-        snap.forEach(function (doc) {
-            var n = doc.data().name;
-            var r = doc.data().review;
-            var d = doc.data().date;
-            console.log(n);
-            var newdom = "<p> " + n + "<br>" + r + "<br>" + d + "</p>";
-            $("#reviews-goes-here").append(newdom);
-            //document.getElementById("cities-go-here").innerHTML = newdom;
+            snap.forEach(function(doc) {
+                var n = doc.data().name; //username
+                var r = doc.data().review; //review
+                var d = doc.data().date; //date
+
+                var codestring = '<div class="customer-reviews">' +
+                '<p class="reviewtext">' + r + '</p>'
+                + '<div class="whoposted">' 
+                + '<div class="name">' + n + '</div>'
+                + '<div class="timeposted">' + d + '</div>'
+                + '</div>'
+                + '</div>';
+
+                //append with jquery to DOM
+                $("#reviews-goes-here").append(codestring);
+            })
         })
-    })
 }
+showReviews();
 
 reviewsQuery();
 
