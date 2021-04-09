212bb1d37863f6ce77d4c128e558a99ad1e4aa5e
Maggie Dou <maggie.d0228@hotmail.com>
2021-04-09 09:30:13 -0700

    fixed menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,7 +9,7 @@ function writeMenus() {
 //     .then(function(snap){
 //         snap.forEach(function(doc){
 //             var n = doc.data().name;
-            
+
 //             console.log(n);
 //             var newdom = "<p> " + n + "</p>";
 //             $("#menus-goes-here").append(newdom);
@@ -18,28 +18,31 @@ function writeMenus() {
 // }
 // menusQuery();
 
-function showMenuCollection(){
+function showMenuCollection() {
     db.collection("menu")
-		.get()    //get whole collection
-    .then(function(snap){
-        snap.forEach(function(doc){          //cycle thru each doc 
-            // do something with each document
-            var pic = doc.data().imgURL;   //key "picture"
-            var item = doc.data().menu_item;    //key "menu_item"
-            var descrip = doc.data().description; // key "description"
-            var price = doc.data().price;
-            // construct the string for card
-            var codestring = '<div class="card">' +
-          '<img src="images/' + pic + '"class="card-img-top" alt="...">' +
-          '<div class="card-body">' +
-            '<h5 class="card-title">' + item + '</h5>' +
-            '<p>' + descrip + '</p>' +
-            '<p> price: ' + price + '</p>' + '</div>' +
-            '<div class="form-check>'
-            + '<input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">'
-            + '<label class="form-check-label" for="flexCheckIndeterminate">Add to cart</label></div>'
-            $("#menus-goes-here").append(codestring);
+        .get()    //get whole collection
+        .then(function (snap) {
+            snap.forEach(function (doc) {          //cycle thru each doc 
+                // do something with each document
+                var pic = doc.data().imgURL;   //key "picture"
+                var item = doc.data().menu_item;    //key "menu_item"
+                var descrip = doc.data().description; // key "description"
+                var price = doc.data().price;
+                // construct the string for card
+                var codestring = '<div class="card">' +
+                    '<img src="images/' + pic + '"class="card-img-top" alt="...">' +
+                    '<div class="card-body">' +
+                    '<h5 class="card-title">' + item + '</h5>' +
+                    '<p>' + descrip + '</p>' +
+                    '<p> price: ' + price + '</p>' + '</div>' +
+                    '<div class="form-check">' +
+                    '<input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">' +
+                    '<label class="form-check-label" for="flexCheckIndeterminate">' +
+                    'Add to Cart' +
+                    '</label>' +
+                    '</div>';
+                $("#menus-goes-here").append(codestring);
+            })
         })
-    })
 }
 showMenuCollection();
